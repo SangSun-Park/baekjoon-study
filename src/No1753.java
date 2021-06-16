d8a18cb60a3c8e195db09afe3089d7be8fe5f9ef
SangSunPark <parkss9677@gmail.com>
2021-06-17 01:34:51 +0900

    Update No1753.java

--- src/No1753.java
+++ src/No1753.java
@@ -1,6 +1,22 @@
+import java.util.*;
+
 public class No1753 {
 
+    static int v, e, k;
+    static int[] dist;
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String[] st = scanner.nextLine().split(" ");
+        v = Integer.parseInt(st[0]);
+        e = Integer.parseInt(st[1]);
+        k = scanner.nextInt();
+        dist = new int[v+1];
+
+        Arrays.fill(dist, -1);
+        dist[k] = 0;
+        
 
     }
 }
