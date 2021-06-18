629a8a7ede04c4f79aff1376a5f3b445f9d06a80
SangSun-Park <parkss9677@gmail.com>
2021-06-19 02:02:12 +0900

    clear

---
+++ src/No9251.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class No9251 {
+
+    static char[] str1;
+    static char[] str2;
+
+    static Integer[][] dp;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        str1 = scanner.nextLine().toCharArray();
+        str2 = scanner.nextLine().toCharArray();
+
+        dp = new Integer[str1.length][str2.length];
+
+        System.out.println(LCS(str1.length - 1, str2.length - 1));
+    }
+
+    static int LCS(int x, int y){
+
+        if (x == -1 || y == -1){
+            return 0;
+        }
+
+        if (dp[x][y] == null){
+            if (str1[x] == str2[y]){
+                dp[x][y] = LCS(x-1, y-1) + 1;
+            }
+            else {
+                dp[x][y] = Math.max(LCS(x-1, y), LCS(x, y-1));
+            }
+        }
+
+        return dp[x][y];
+    }
+}
