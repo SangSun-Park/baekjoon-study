64d4b94279917b3fc660ab7fea418c32f1212c9b
SangSunPark <parkss9677@gmail.com>
2021-06-21 00:39:42 +0900

    update

--- src/No14503.java
+++ src/No14503.java
@@ -4,6 +4,7 @@ public class No14503 {
 
     static int n, m, r, c, d;
     static int[][] room;
+    static int[] dx = {-1, 0, 1, 0}, dy = {0 , 1, 0, -1};
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -33,17 +34,16 @@ public class No14503 {
             return answer;
 
         room[r][c] = 2;
-
         return findAnswer(answer + 1);
     }
 
     static boolean check(){
-        for (int i = 0; i < 4; i++){
+        for (int i = 4; i > 0; i--){
             if(turn()) {
-                d = (d + i) % 4;
+                d = (d + 1) % 4;
                 return true;
             }
-            d = (d + i) % 4;
+            d = (d + 1) % 4;
         }
         if(move())
             check();
@@ -53,25 +53,25 @@ public class No14503 {
 
     static boolean move(){
         if (d == 0){
-            if (room[r + 1][c] == 2) {
+            if (room[r + 1][c] != 1) {
                 r += 1;
                 return true;
             }
         }
         else if (d == 1){
-            if (room[r][c - 1] == 2){
+            if (room[r][c - 1] != 1){
                 c -= 1;
                 return true;
             }
         }
         else if (d == 2){
-            if (room[r - 1][c] == 2){
+            if (room[r - 1][c] != 1){
                 r -= 1;
                 return true;
             }
         }
         else if (d == 3){
-            if (room[r][c + 1] == 2){
+            if (room[r][c + 1] != 1){
                 c += 1;
                 return true;
             }
@@ -81,26 +81,26 @@ public class No14503 {
 
     static boolean turn(){
         if (d == 0){
-            if (room[r][c + 1] == 0) {
-                c += 1;
-                return true;
-            }
-        }
-        else if (d == 1){
-            if (room[r + 1][c] == 0){
-                r += 1;
-                return true;
-            }
-        }
-        else if (d == 2){
-            if (room[r][c - 1] == 0){
+            if (room[r][c - 1] == 0) {
                 c -= 1;
                 return true;
             }
         }
-        else if (d == 3){
+        else if (d == 1){
             if (room[r - 1][c] == 0){
                 r -= 1;
+                return true;
+            }
+        }
+        else if (d == 2){
+            if (room[r][c + 1] == 0){
+                c += 1;
+                return true;
+            }
+        }
+        else if (d == 3){
+            if (room[r + 1][c] == 0){
+                r += 1;
                 return true;
             }
         }
