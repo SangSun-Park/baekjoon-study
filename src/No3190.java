dbd542ea413d9b41b1646d5eb30a1bdd86046e6e
SangSunPark <parkss9677@gmail.com>
2021-06-28 00:19:40 +0900

    Create No3190.java

---
+++ src/No3190.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class No3190 {
+
+    static int n, k, l;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = Integer.parseInt(scanner.nextLine());
+        k = Integer.parseInt(scanner.nextLine());
+        int x, y;
+        String[] st;
+        for(int i = 0; i < k; i++){
+            st = scanner.nextLine().split(" ")
+            x = Integer.parseInt(st[0]);
+            y = Integer.parseInt(st[1]);
+        }
+        l = Integer.parseInt(scanner.nextLine());
+
+    }
+}
