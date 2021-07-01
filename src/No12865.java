b2fab082c34321b481a7494f80e9d2255a3c0a3d
SangSunPark <parkss9677@gmail.com>
2021-07-02 02:06:10 +0900

    clear

--- src/No12865.java
+++ src/No12865.java
@@ -12,22 +12,22 @@ public class No12865 {
         n = sc.nextInt();
         k = sc.nextInt();
 
-        dp = new int[n+1][k+1];
+        dp = new int[n + 1][k + 1];
 
-        w = new int[n+1];
-        v = new int[n+1];
+        w = new int[n + 1];
+        v = new int[n + 1];
 
-        for(int i=1;i<=n;i++) {
+        for(int i = 1; i <= n; i++) {
             w[i] = sc.nextInt();
-            v[i]= sc.nextInt();
+            v[i] = sc.nextInt();
 
         }
 
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=k;j++) {
-                dp[i][j] = dp[i-1][j];
-                if(j - w[i]>=0) {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= k; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if(j - w[i] >= 0) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]]+v[i]);
                 }
             }
         }
